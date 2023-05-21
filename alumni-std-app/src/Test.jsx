1fa916fee6f17816f1ee9f4a7473d31c469d3638
Kavi <kavialin23@gmail.com>
2023-05-21 11:27:48 +0300

    First Push 5/21/23

---
+++ alumni-std-app/src/Test.jsx
@@ -0,0 +1,20 @@
+import React,{useState} from "react";
+
+
+export default function()
+{
+    const [addUser,setAddUser] = useState("")
+
+    function AddUse()
+    {
+
+    }
+    return(
+        <div>
+            <input type="text" name="user" id="user" />
+            <ul>
+                <li></li>
+            </ul>
+        </div>
+    )
+}
