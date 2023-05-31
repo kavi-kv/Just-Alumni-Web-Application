40483828553146f097beba1bb9ff5072be338fb7
Kavi <kavialin23@gmail.com>
2023-05-31 16:16:06 +0300

    changes 2

--- alumni-std-app/src/main.jsx
+++ alumni-std-app/src/main.jsx
@@ -1,15 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import Welcome from './UI/Home/WelcomePage'
-import Registration from './UI/Home/Registration'
-import Login from './UI/Login/Login'
-import Team from './UI/Home/Team'
-import data from './UI/Home/Team.json'
-import Gallary from './UI/Home/Gallary'
-
-import ReactDOM from "react-dom/client";
-import App from "./App";
 import Welcome from "./UI/Home/WelcomePage";
 import Registration from "./UI/Home/Registration";
 import Login from "./UI/Login/Login";
